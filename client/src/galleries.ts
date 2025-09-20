09a3b4383ae8cb2b75c004077351139236ffee0c
mtbc1947 <mtbc1947@gmail.com>
2025-09-20 17:37:49 +0800

    photos 3

--- client/src/galleries.ts
+++ client/src/galleries.ts
@@ -9,29 +9,29 @@ const galleries: Gallery[] = [
   {
     id: 1,
     title: "Captains v Presidents 2025",
-    cover: "src/photos/Capt_Pres_2025/Capt Pres 2025 (2).jpg",
+    cover: "/public/photos/CaptPres2025/Capt Pres 2025 (2).jpg",
     photos: [
-      {url: "src/photos/Capt_Pres_2025/Capt Pres 2025 (1).jpg"},
-      {url: "src/photos/Capt_Pres_2025/Capt Pres 2025 (3).jpg"},
-      {url: "src/photos/Capt_Pres_2025/Capt Pres 2025 (4).jpg"},
+      {url: "/public/photos/CaptPres2025/Capt Pres 2025 (1).jpg"},
+      {url: "/public/photos/CaptPres2025/Capt Pres 2025 (3).jpg"},
+      {url: "/public/photos/CaptPres2025/Capt Pres 2025 (4).jpg"},
     ],
   },
   {
     id: 2,
-    title: "Final Sat 2025",
-    cover: "src/photos/Finals_Sat_2025/Finals Saturday 2025 (1).jpg",
+    title: "Finals Sat 2025",
+    cover: "/public/photos/FinalsSat2025/Finals Saturday 2025 (1).jpg",
     photos: [
-      {url: "src/photos/Finals_Sat_2025/Finals Saturday 2025 (2).jpg"},
-      {url: "src/photos/Finals_Sat_2025/Finals Saturday 2025 (3).jpg"},
+      {url: "/public/photos/FinalsSat2025/Finals Saturday 2025 (2).jpg"},
+      {url: "/public/photos/FinalsSat2025/Finals Saturday 2025 (3).jpg"},
     ],
   },
   {
     id: 3,
     title: "Finals Sun 2025",
-    cover: "src/photos/Finals_Sunday_2025/1R7A8392.jpg",
+    cover: "/public/photos/FinalsSun2025/1R7A8392.jpg",
     photos: [
-      {url: "src/photos/Finals_Sunday_2025/1R7A8560.jpg"},
-      {url: "src/photos/Finals_Sunday_2025/1R7A8563.jpg"},
+      {url: "/public/photos/FinalsSun2025/1R7A8560.jpg"},
+      {url: "/public/photos/FinalsSun2025/1R7A8563.jpg"},
     ],
   },
 ];
